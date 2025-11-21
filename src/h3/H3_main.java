314cf5aa1f2e3c4ebc3248c183d875156406be87
Finn <todotwt99@gmail.com>
2025-11-21 18:59:48 +0100

    Completed the assignment

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -7,7 +7,7 @@ public class H3_main {
     public static void main(String[] args){
 
         int[][] einheiten ={{50000,20000,10000,5000,2000,1000,500,200,100,50,20,10,5,2,1},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
-        int input=100000;
+        int input=1000000;
 
 
         for (int i = 0; i < einheiten[0].length; i++) {

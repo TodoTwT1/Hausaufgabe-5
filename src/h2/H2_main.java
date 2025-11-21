24cb78e90c7bf063ef3edf533609eed527128b72
Finn <todotwt99@gmail.com>
2025-11-21 18:57:42 +0100

    Completed the assignment

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,28 @@
 package h2;
 
+import java.util.Arrays;
+import java.util.Set;
+
 public class H2_main {
+    public static void main(String[] args) {
+
+        int n = 299;
+        int digits = 12;
+        int[] a = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        digits = 0;
+
+        if (n == 0) {
+            digits = 1;
+        } else
+            while (n > 0) {
+                a[8-digits]=n%10;
+                n = n / 10;
+                digits++;
+            }
+
+                System.out.println(digits);
+            System.out.println(Arrays.toString(a));
+
+    }
 }
---
+++ src/h3/H3_main.java
@@ -0,0 +1,28 @@
+package h3;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class H3_main {
+    public static void main(String[] args){
+
+        int[][] einheiten ={{50000,20000,10000,5000,2000,1000,500,200,100,50,20,10,5,2,1},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+        int input=100000;
+
+
+        for (int i = 0; i < einheiten[0].length; i++) {
+            if (input >= einheiten[0][i]) {
+                einheiten[1][i] = input / einheiten[0][i];
+                input = input % einheiten[0][i];
+            }
+        }
+
+        System.out.println(Arrays.toString(einheiten[0]));
+        System.out.println(Arrays.toString(einheiten[1]));
+
+
+
+
+
+    }
+}
